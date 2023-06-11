b06f9ad0decbe31ed8b88a6e695ed6df2548a47c
Thayssa Ferreira <thayssa2000.3@gmail.com>
2023-06-10 22:57:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const list = document.querySelectorAll('.list');
+function activeLink() {
+  list.forEach((item) =>
+    item.classList.remove('active'));
+  this.classList.add('active');
+}
+list.forEach((item) =>
+  item.addEventListener('click', activeLink));
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+
+    setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
